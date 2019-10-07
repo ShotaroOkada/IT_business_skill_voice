0ad7ae5fbbe5e6cdc3e8b72f112305bb8c8cec8f
ShotaroOkada <b1016117@gmail.com>
2019-10-07 10:56:06 +0900

    何故か動かないバグを修正

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express()
 const path = require('path')
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/index.html', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
+app.use('/static', express.static('static'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/index.html', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
